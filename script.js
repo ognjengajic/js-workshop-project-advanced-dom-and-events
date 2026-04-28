2bffe78c88dce6929accfc29e5ea34d60cc7f359
ognjengajic <ognjeng535@gmail.com>
2026-04-28 11:45:43 +0200

    Implementing nav links fade animation

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const scrollButton = document.querySelector(`.btn--scroll-to`);
 const section1 = document.querySelector(`#section--1`);
+const nav = document.querySelector(`.nav`);
+//Tabbed component
+const tabs = document.querySelectorAll(`.operations__tab`);
+const tabContainer = document.querySelector(`.operations__tab-container`);
+const tabsContent = document.querySelectorAll(`.operations__content`);
 
 ///////////////////////////////////////
 // Modal window
@@ -89,11 +94,6 @@ document.querySelector(`.nav__links`).addEventListener(`click`, function (e) {
   }
 });
 
-//Tabbed component
-const tabs = document.querySelectorAll(`.operations__tab`);
-const tabContainer = document.querySelector(`.operations__tab-container`);
-const tabsContent = document.querySelectorAll(`.operations__content`);
-
 tabContainer.addEventListener(`click`, function (e) {
   const clicked = e.target.closest(`.operations__tab`);
   //console.log(clicked);
@@ -112,6 +112,47 @@ tabContainer.addEventListener(`click`, function (e) {
     .classList.add('operations__content--active');
 });
 
+//Implementing nav links fade animation
+/*
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains(`nav__link`)) {
+    const link = e.target;
+    const siblings = link.closest(`.nav`).querySelectorAll(`.nav__link`);
+    const logo = link.closest(`.nav`).querySelector(`img`);
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = opacity;
+      }
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener(`mouseover`, function (e) {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener(`mouseout`, function (e) {
+  handleHover(e, 1);
+});
+*/
+const handleHover = function (e) {
+  if (e.target.classList.contains(`nav__link`)) {
+    const link = e.target;
+    const siblings = link.closest(`.nav`).querySelectorAll(`.nav__link`);
+    const logo = link.closest(`.nav`).querySelector(`img`);
+    siblings.forEach(el => {
+      if (el !== link) {
+        el.style.opacity = this;
+      }
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener(`mouseover`, handleHover.bind(0.5));
+
+nav.addEventListener(`mouseout`, handleHover.bind(1));
 /////////////////
 /////////////////
 /////////////////
